ff4065eb5ffe16e5422615fa9f41ff105f54cc17
Savita Kumbhare <58700287+savitakumbhare@users.noreply.github.com>
2021-09-30 18:40:55 +0530

    Separate methods for filtering

--- todo.rb
+++ todo.rb
@@ -2,38 +2,51 @@ require "active_record"
 require "date"
 
 class Todo < ActiveRecord::Base
+  NO_DATE = " "
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where("due_date =?", Date.today)
+  end
+
+  def self.due_later
+    where("due_date >?", Date.today)
+  end
+
   def self.show_list
-    
     puts "My Todo-list\n\n"
 
     puts "Overdue\n"
-    Todo.where("due_date < ?", Date.today).map { |todo| puts "#{todo.to_displayable_string}" }
+    puts overdue.map { |todo| todo.to_displayable_string }
     puts "\n\n"
 
     puts "Due Today\n"
-    Todo.where("due_date = ?", Date.today).map { |todo| puts "#{todo.to_displayable_string}" }
+    puts due_today.map { |todo| todo.to_displayable_string }
 
     puts "\n\n"
 
     puts "Due Later\n"
-    Todo.where("due_date > ?", Date.today).map { |todo| puts "#{todo.to_displayable_string}" }
+    puts due_later.map { |todo| todo.to_displayable_string }
   end
 
   def self.add_task(h)
     Todo.create!(todo_text: h[:todo_text], due_date: Date.today + h[:due_in_days], completed: false)
   end
 
+  def is_due_today?
+    due_date == Date.today
+  end
+
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
-    "#{id}. #{display_status} #{todo_text}"
+    display_date = "#{is_due_today? ? " " : due_date}"
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
   end
 
   def is_completed?
     Todo.where(completed == true)
-  end
-
-  def due_today?
-    due_date == Date.today
   end
 
   def self.mark_as_complete(todo_id)
